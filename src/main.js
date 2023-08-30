a6e07fad4ba338eee2b9653ae4b577c886acab55
jhonatan <jhobatrix@gmail.com>
2023-08-30 13:02:05 -0500

    Se crea variable de entorno, y carpetas del proyecto

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 
 import App from './App.vue'
 import router from './router'
+// import store from './store'
 
-import './assets/main.css'
+import './assets/main.scss'
 
 new Vue({
   router,
